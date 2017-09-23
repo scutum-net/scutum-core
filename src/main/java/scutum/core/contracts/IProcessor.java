8ae2b8464d40b561d114cacba72e0fc2d787dae7
Dima Statz <dima.statz@cloudlock.com>
2017-09-23 12:59:58 +0300

    Refactor common interfaces

--- src/main/java/scutum/core/contracts/IProcessor.java
+++ src/main/java/scutum/core/contracts/IProcessor.java
@@ -1,5 +1,6 @@
 package scutum.core.contracts;
 
 public interface IProcessor {
+    Integer getProviderId();
     Alert[] process(ScannedData data);
 }
--- src/main/java/scutum/core/contracts/ScannedData.java
+++ src/main/java/scutum/core/contracts/ScannedData.java
@@ -3,20 +3,30 @@ package scutum.core.contracts;
 import java.util.Dictionary;
 
 public class ScannedData {
+    private final Integer customerId;
     private final Integer providerId;
     private final String providerName;
-    private final Integer machineId;
-    private final Integer scanId;
+
     private final String data;
+    private final Integer scanId;
+    private final Integer machineId;
 
-
-    public ScannedData(Integer providerId, String providerName, Integer machineId, Integer scanId, String data) {
+    public ScannedData(Integer customerId,
+                       Integer providerId,
+                       String providerName,
+                       Integer machineId,
+                       Integer scanId,
+                       String data) {
+        // customer id
+        this.customerId = customerId;
         this.providerId = providerId;
         this.providerName = providerName;
         this.machineId = machineId;
         this.scanId = scanId;
         this.data = data;
     }
+
+    public Integer getCustomerId() { return customerId; }
 
     public Integer getProviderId() {
         return providerId;
